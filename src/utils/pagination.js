7ea43af35eabbf153b0d30cf96c77f9eef71d44e
Leo Gavin <leogavin123@outlook.com>
2024-07-04 12:25:13 +0530

    Update pagination.js

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,6 +1,6 @@
 const { ActionRowBuilder, ButtonBuilder } = require('discord.js');
 
-function paginationEmbed(interaction, pages, emojiList = ['First', 'Last'], timeout = 120000) {
+async function paginationEmbed(interaction, pages, emojiList = ['First', 'Last'], timeout = 120000) {
   if (!interaction || !pages) throw new Error('interaction and pages are required.');
   if (!Array.isArray(pages)) throw new Error('pages must be an array.');
 
@@ -26,12 +26,14 @@ function paginationEmbed(interaction, pages, emojiList = ['First', 'Last'], time
         .setStyle('Secondary'),
     );
 
-  interaction.reply({ embeds: [pages[page]], components: [row], fetchReply: true ,ephemeral: true}).then(curPage => {
-    const filter = i => i.customId === 'first' || i.customId === 'previous' || i.customId === 'next' || i.customId === 'last';
+  await interaction.reply({ embeds: [pages[page]], components: [row], ephemeral: true });
 
-    const collector = curPage.createMessageComponentCollector({ filter, time: timeout });
+  const filter = i => ['first', 'previous', 'next', 'last'].includes(i.customId) && i.user.id === interaction.user.id;
 
-    collector.on('collect', async i => {
+  const collector = interaction.channel.createMessageComponentCollector({ filter, time: timeout });
+
+  collector.on('collect', async i => {
+    try {
       switch (i.customId) {
         case 'first':
           page = 0;
@@ -47,38 +49,44 @@ function paginationEmbed(interaction, pages, emojiList = ['First', 'Last'], time
           break;
       }
       await i.deferUpdate();
-      await i.editReply({ embeds: [pages[page]], components: [row] , ephemeral: true});
+      await i.followUp({ embeds: [pages[page]], components: [row], ephemeral: true });
       collector.resetTimer();
-    });
+    } catch (error) {
+      console.error('Error sending new message:', error);
+    }
+  });
 
-    collector.on('end', (_, reason) => {
-      if (reason !== 'messageDelete') {
-        const disabledRow = new ActionRowBuilder()
-          .addComponents(
-            new ButtonBuilder()
-              .setCustomId('first')
-              .setLabel('First')
-              .setStyle('Secondary')
-              .setDisabled(true),
-            new ButtonBuilder()
-              .setCustomId('previous')
-              .setLabel('Previous')
-              .setStyle('Secondary')
-              .setDisabled(true),
-            new ButtonBuilder()
-              .setCustomId('next')
-              .setLabel('Next')
-              .setStyle('Secondary')
-              .setDisabled(true),
-            new ButtonBuilder()
-              .setCustomId('last')
-              .setLabel('Last')
-              .setStyle('Secondary')
-              .setDisabled(true),
-          );
-        curPage.edit({ embeds: [pages[page]], components: [disabledRow] , ephemeral: true});
+  collector.on('end', async (_, reason) => {
+    if (reason !== 'messageDelete') {
+      const disabledRow = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId('first')
+            .setLabel('First')
+            .setStyle('Secondary')
+            .setDisabled(true),
+          new ButtonBuilder()
+            .setCustomId('previous')
+            .setLabel('Previous')
+            .setStyle('Secondary')
+            .setDisabled(true),
+          new ButtonBuilder()
+            .setCustomId('next')
+            .setLabel('Next')
+            .setStyle('Secondary')
+            .setDisabled(true),
+          new ButtonBuilder()
+            .setCustomId('last')
+            .setLabel('Last')
+            .setStyle('Secondary')
+            .setDisabled(true),
+        );
+      try {
+        await interaction.followUp({ embeds: [pages[page]], components: [disabledRow], ephemeral: true });
+      } catch (error) {
+        console.error('Error sending final disabled message:', error);
       }
-    });
+    }
   });
 }
 
